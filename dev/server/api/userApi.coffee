ed862d380577bde1285aefcebc7007e35268a621
Gianluigi Mango <Luigi.Mango@Kindred.local>
2016-05-04 17:27:44 +0100

    added api method to find user

--- dev/server/api/userApi.coffee
+++ dev/server/api/userApi.coffee
@@ -3,39 +3,39 @@ User = require './../models/userModel'
 
 # @this.path[0] = key, @this.path[1] = value
 module.exports = (action, path, res) ->
-	@path = path
-	@action = action
-
 	console.info '[Query]', action
 
 	getInfo = ->
-		User.findOne userid: @path[0][1], (err, body) ->
-			if err
-				console.log err
-			else
-				if body then res.send data: body, response: true else res.send data: 'Not found', response: false
+		findUser path[0][1], (exists) ->
+			if exists then res.send data: exists, response: true else res.send data: 'User does not exist', response: false
 
 	addUser = ->
 		user = new User
-			userid: @path[0][1],
-			password: @path[1][1],
+			userid: path[0][1],
+			password: path[1][1],
 			active: 1
 
-		user.save (err, body) ->
-			if err 
-				console.log err
-			else 
-				if body then res.send data: body, response: true else res.send response: false
+		findUser user.userid, (exists) ->
+			if exists
+				res.send data: 'User already exists', response: false
+			else
+				user.save (err, body) ->
+					unless err then (if body then res.send data: body, response: true else res.send response: false) else console.log err
 
-	deleteuser = ->
-		User.remove _id: @path[0][1], (err, body) ->
+	deleteUser = ->
+		User.remove _id: path[0][1], (err, body) ->
 			if err
 				console.log err
 			else
 				if body then res.send data: 'Deleted successfully', response: true else res.send data: 'Not found', response: false
 
+	# @private function @return cb
+	findUser = (user, cb) ->
+		User.find userid: user, (err, docs) ->
+			unless err then (if docs.length then cb docs else cb false) else console.log err	
+
 	switch action
 		when 'getInfo' then getInfo()
 		when 'addUser' then addUser()
-		when 'deleteUser' then deleteuser()
+		when 'deleteUser' then deleteUser()
 		else res.send Error: 'No method found'
--- dist/server/api/userApi.js
+++ dist/server/api/userApi.js
@@ -4,58 +4,59 @@
   User = require('./../models/userModel');
 
   module.exports = function(action, path, res) {
-    var addUser, deleteuser, getInfo;
-    this.path = path;
-    this.action = action;
+    var addUser, deleteUser, findUser, getInfo;
     console.info('[Query]', action);
     getInfo = function() {
-      return User.findOne({
-        userid: this.path[0][1]
-      }, function(err, body) {
-        if (err) {
-          return console.log(err);
+      return findUser(path[0][1], function(exists) {
+        if (exists) {
+          return res.send({
+            data: exists,
+            response: true
+          });
         } else {
-          if (body) {
-            return res.send({
-              data: body,
-              response: true
-            });
-          } else {
-            return res.send({
-              data: 'Not found',
-              response: false
-            });
-          }
+          return res.send({
+            data: 'User does not exist',
+            response: false
+          });
         }
       });
     };
     addUser = function() {
       var user;
       user = new User({
-        userid: this.path[0][1],
-        password: this.path[1][1],
+        userid: path[0][1],
+        password: path[1][1],
         active: 1
       });
-      return user.save(function(err, body) {
-        if (err) {
-          return console.log(err);
+      return findUser(user.userid, function(exists) {
+        if (exists) {
+          return res.send({
+            data: 'User already exists',
+            response: false
+          });
         } else {
-          if (body) {
-            return res.send({
-              data: body,
-              response: true
-            });
-          } else {
-            return res.send({
-              response: false
-            });
-          }
+          return user.save(function(err, body) {
+            if (!err) {
+              if (body) {
+                return res.send({
+                  data: body,
+                  response: true
+                });
+              } else {
+                return res.send({
+                  response: false
+                });
+              }
+            } else {
+              return console.log(err);
+            }
+          });
         }
       });
     };
-    deleteuser = function() {
+    deleteUser = function() {
       return User.remove({
-        _id: this.path[0][1]
+        _id: path[0][1]
       }, function(err, body) {
         if (err) {
           return console.log(err);
@@ -74,13 +75,28 @@
         }
       });
     };
+    findUser = function(user, cb) {
+      return User.find({
+        userid: user
+      }, function(err, docs) {
+        if (!err) {
+          if (docs.length) {
+            return cb(docs);
+          } else {
+            return cb(false);
+          }
+        } else {
+          return console.log(err);
+        }
+      });
+    };
     switch (action) {
       case 'getInfo':
         return getInfo();
       case 'addUser':
         return addUser();
       case 'deleteUser':
-        return deleteuser();
+        return deleteUser();
       default:
         return res.send({
           Error: 'No method found'
